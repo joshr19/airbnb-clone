1a9b74edc0866f51bec76d0f2e3d83b6ab2f79ab
joshr19 <89062549+joshr19@users.noreply.github.com>
2021-09-06 11:28:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './Home'
 import Header from './Header';
 import Footer from './Footer';
 import SearchPage from './SearchPage';
-import {HashRouter as Router , Switch, Route} from "react-router-dom";
+import {BrowseRouter as Router , Switch, Route} from "react-router-dom";
 
 function App() {
   return (
